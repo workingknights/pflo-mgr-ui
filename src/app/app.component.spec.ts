b12c86bcd4459bc11d620cdfaf927e25164b933f
workingknights <anthony.knights@gmail.com>
2017-03-03 10:05:28 +0900

    Enabled Jasmine tests on Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Auth } from './auth.service';
+
+class MockAuth { public authenticated() { return true }; }
+
 describe('App', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({
+			declarations: [AppComponent],
+			providers: [ {provide: Auth,  useClass: MockAuth }],
+			imports: [ RouterTestingModule ]
+		});
   });
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
